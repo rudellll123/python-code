35eb9626d79a4e522608d1c5b4f9f347bf16f108
RAHUL JHA <rj2544@srmist.edu.in>
2026-02-21 09:52:44 +0000

    push

--- python basics/.py
+++ python basics/.py
@@ -1,3 +1,14 @@
 
 # 1 rectangle area calc
 
+
+
+# 3 madlibs game
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb ending with 'ing'")
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
---
+++ python basics/airthmetic.py
@@ -0,0 +1,52 @@
+
+#friends = 5
+#friends = friends + 1
+#friends += 1
+#friends = friends * 12
+#friends *= 12
+
+
+#print(friends)
+
+
+
+
+
+
+
+#import math
+
+
+#x = 9
+#print(math.pi)
+#print(math.e)
+#result = math.floor(x)
+#print(result)
+
+# calculate circumference of the circle
+import math
+radius = float(input('Enter the radius of a circle: '))
+circumference = 2 * math.pi * radius
+print(f"The circumference is:{circumference}")
+
+
+
+# area of the circle
+import math
+radius = float(input("Enter the radius of a circle"))
+area = math.pi * pow(radius,2)
+print(f"The area of the circle is: {round(area,2)}cm^2")
+
+
+#hypotenuse of a right angle triangle
+import math
+
+a = float(input("Enter side A: "))
+b = float(input("Enter side B: "))
+
+C = math.sqrt(pow(a,2) + pow(b,2))
+
+print(f"Side C = {c}")
+
+
+

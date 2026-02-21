d63583a815a83f791b9694f885d00ae2541cbe19
RAHUL JHA <rj2544@srmist.edu.in>
2026-02-21 09:01:40 +0000

    push

---
+++ python basics/.py
@@ -0,0 +1,3 @@
+
+# 1 rectangle area calc
+
---
+++ python basics/re.py
@@ -0,0 +1,51 @@
+#variabless
+#string
+first_name = "rj"
+food ="pizza" 
+email ="ewad"
+print(f"Hello {first_name}")
+print(f"burger{food}")
+print(f"email{email}")
+
+
+#integers
+age = 15
+no_of_students = 16
+print(f"you are {age} years old ")
+print(f"in class there is {no_of_students}")
+
+
+#float
+price = 10.99
+cgpa = 9.8
+distance = 98
+print(f"the price is ${price} ")
+print(f"the students scored{cgpa}")
+print(f"i ran{distance}km")
+
+#boolean
+is_student = True
+is_online = False
+if is_online:
+        print("you are online")
+else:
+      print("you are offline")
+
+
+#typecasting
+name = "rahul jha"
+age = 14
+gpa = 3.9
+is_student = True
+
+print(type(name))
+age = float(age)
+print (age)
+
+#input ()
+
+name = input("What is your name?:")
+old = input("How old are you?: ")
+
+print(f"you are{old}")
+print(f"Hello {name}~")
